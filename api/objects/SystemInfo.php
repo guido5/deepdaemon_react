0ff3e64627276b56039d30215c08c5ce951aeff1
Diego Guido <dguidor@gmail.com>
2020-04-02 17:47:24 -0600

    URL's actualizadas

--- api/objects/SystemInfo.php
+++ api/objects/SystemInfo.php
@@ -2,15 +2,19 @@
 class SystemInfo {
     static $urlConection = "//api.deepdaemon.org/";
     //static $urlConection = "//localhost/deepdaemon_react/api/";
+    static $urlAdminConection = "http://api.deepdaemon.org/";
+    //static $urlAdminConection = "http://localhost:3001/";
     static $host               = "deepdaemon.org";
     static $db_name            = "deepdaem_web";
     static $username           = "deepdaem_web";
-    static $password           = "LATLpc5y49i2";                          //Falta agregar la contraseña de produccion
+    static $password           = "LATLpc5y49i2";
     /*static $host         = "localhost";
     static $db_name      = "deepdaem_web";
     static $username     = "root";
     static $password     = "";*/
 
     static $path = '/home/deepdaem/public_html/media/team/small/';
+    static $path_project = '/home/deepdaem/public_html/media/projects/';
     //static $path = '../public/media/team/small/';
+    //static $path_project = '../public/media/projects/';
 }
--- src/components/System.js
+++ src/components/System.js
@@ -2,8 +2,8 @@ import React from "react";
 
 class System extends React.Component {
     
-    static urlConection = "//api.deepdaemon.org/";
-    //static urlConection = "//localhost/deepdaemon_react/api/";
+    //static urlConection = "//api.deepdaemon.org/";
+    static urlConection = "//localhost/deepdaemon_react/api/";
     static projects = "projects/";
     static members = "members/";
 
